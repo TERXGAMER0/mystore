7614a9c6620a09b32f39c13adedea7df6ae4df7b
TERXGAMER0 <TerXGamer123@gmail.com>
2026-04-28 17:27:22 +0300

    Update run.py

--- run.py
+++ run.py
@@ -4,10 +4,10 @@ from dotenv import load_dotenv
 
 load_dotenv(encoding='utf-8')
 
-from bot import start_bot
+from bot.main import main
 
 if __name__ == "__main__":
     try:
-        asyncio.run(start_bot())
+        asyncio.run(main())
     except (KeyboardInterrupt, SystemExit):
         logging.info("Bot stopped.")
